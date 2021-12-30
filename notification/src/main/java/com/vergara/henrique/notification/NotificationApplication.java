193ae5f2dc2aaff24e24a23b9d8bc4b38f1907aa
Henrique <henrique.vergara.hv@gmail.com>
2021-12-30 17:14:22 -0300

    Implementing Rabbit Listener to consume messages from the topic

--- notification/src/main/java/com/vergara/henrique/notification/NotificationApplication.java
+++ notification/src/main/java/com/vergara/henrique/notification/NotificationApplication.java
@@ -1,10 +1,7 @@
 package com.vergara.henrique.notification;
 
-import com.vergara.henrique.amqp.RabbitMQMessageProducer;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(
     scanBasePackages = {
@@ -17,19 +14,4 @@ public class NotificationApplication {
   public static void main(String[] args) {
     SpringApplication.run(NotificationApplication.class, args);
   }
-
-    @Bean
-    CommandLineRunner commandLineRunner(
-            RabbitMQMessageProducer producer,
-            NotificationConfig notificationConfig
-            ) {
-        return args -> {
-            producer.publish(
-                    new Person("Ali", 18),
-                    notificationConfig.getInternalExchange(),
-                    notificationConfig.getInternalNotificationRoutingKey());
-        };
-    }
-
-    record Person(String name, int age){}
 }
